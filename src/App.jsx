21e998ce1ed903687da9a280310b4e3f91a40f34
RohitTiz <rohittiwari06052004@gmail.com>
2025-08-07 20:04:33 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+  const [color , setColor] =useState("olive")
   return (
-    <>
-      <div className="flex items-center justify-center h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-600">
-          hello
-        </h1>
+    <div className='w-full h-screen duration-200'
+    style={{backgroundColor: color}}>
+      <div className="fixed flex flex-wap justify-center bottom-12 inset-x-0 px-2">
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+
+          {/* //buttonstart */}
+          <button 
+          onClick={() => setColor("red")}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+          style={{backgroundColor : "red"}}>
+            Red
+          </button>
+          {/* //buttonover */}
+          {/* //buttonstart */}
+          <button
+          onClick={() => setColor("green")}
+           className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+          style={{backgroundColor : "green"}}>
+            green
+          </button>
+          {/* //buttonover */}
+          {/* //buttonstart */}
+          <button
+          onClick={() => setColor("blue")}
+           className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+          style={{backgroundColor : "blue"}}>
+            blue
+          </button>
+          {/* //buttonover */}
+          {/* //buttonstart */}
+          <button 
+          onClick={() => setColor("pink")}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+          style={{backgroundColor : "pink"}}>
+            pink
+          </button>
+          {/* //buttonover */}
+          {/* //buttonstart */}
+          <button
+          onClick={() => setColor("grey")}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+          style={{backgroundColor : "grey"}}>
+            grey
+          </button>
+          {/* //buttonover */}
+          {/* //buttonstart */}
+          <button
+          onClick={() => setColor("orange")}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+          style={{backgroundColor : "orange"}}>
+            orange
+          </button>
+          {/* //buttonover */}
+          {/* //buttonstart */}
+          <button
+          onClick={() => setColor("purple")}
+          className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+          style={{backgroundColor : "purple"}}>
+            purple
+          </button>
+          {/* //buttonover */}
+          
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 

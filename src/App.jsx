bf2db8d547d298208de60777eb43ae1103db8ff1
RohitTiz <rohittiwari06052004@gmail.com>
2025-08-07 19:31:36 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          hello
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
